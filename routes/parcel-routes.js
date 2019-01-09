d1863ff5c9135c2e53df7c9123834c8591220695
Sophyy <Sofiyullahj@gmail.com>
2019-01-09 02:16:09 +0100

    add endpoint to create parcel for authrized user

---
+++ routes/parcel-routes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { createParcel} from '../controllers/parcels-controller.js';
+import { check } from 'express-validator/check';
+import { authorizeUser } from '../middlewares/middleware.js';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+//create a new parcel order.
+app.post('/parcels', authorizeUser, [ 
+  check('recipient_name').isAlphanumeric().withMessage('Name must be letterrs and numbers only'),
+  check('recipient_phone_no')
+  .isNumeric().withMessage('Enter numbers only')
+  .isLength({ max: 11}).withMessage('Phone number cannot be more than 11 digits')
+], createParcel);
+
+export default app;
