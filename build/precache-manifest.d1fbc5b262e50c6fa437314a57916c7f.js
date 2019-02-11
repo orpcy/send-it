0db3e53ebca74d4653f2f1a1076ea0048515e4eb
Sophyy <Sofiyullahj@gmail.com>
2019-02-11 16:58:05 +0100

    add react app

---
+++ build/precache-manifest.d1fbc5b262e50c6fa437314a57916c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e8d307a03d8056c4c925",
+    "url": "/static/js/main.e8d307a0.chunk.js"
+  },
+  {
+    "revision": "3a7a3253ee184f9a14ee",
+    "url": "/static/js/1.3a7a3253.chunk.js"
+  },
+  {
+    "revision": "e8d307a03d8056c4c925",
+    "url": "/static/css/main.281e0c1b.chunk.css"
+  },
+  {
+    "revision": "3a7a3253ee184f9a14ee",
+    "url": "/static/css/1.b84956f5.chunk.css"
+  },
+  {
+    "revision": "615cc9ee792ae503b9c5c5135f2baff6",
+    "url": "/index.html"
+  }
+];
